894a6d0d93f90bb251843a4c6af79c59a20da1ce
Ruben Andres Tubon Coichan <rtubon@RTUBON.local>
2026-01-20 18:58:16 -0500

    cambio clase para hacerla tipo endpoint vuln

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,5 +22,16 @@ public class MainController {
         logger.info("Received a request for API version " + apiVersion);
         return "Hello, world!";
     }
+
+    @GetMapping("/read")
+public String readFile(@RequestParam("file") String fileName) {
+    try {
+        // VULNERABILIDAD: Se toma el nombre del archivo directamente del usuario
+        // sin validar si contiene "../" o rutas absolutas.
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    } catch (IOException e) {
+        return "Error al leer el archivo: " + e.getMessage();
+    }
+}
 
 }
