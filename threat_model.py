eda90060fcb44a231cd8e2085e5cbd5369dbfeed
Ruben Andres Tubon Coichan <rtubon@RTUBON.local>
2026-01-21 18:37:49 -0500

    owasp pytm parcho yml y py

--- threat_model.py
+++ threat_model.py
@@ -1,28 +1,26 @@
 from pytm import TM, Server, Datastore, Dataflow, Boundary
 
 tm = TM("Log4Shell Vulnerable API Model")
-tm.description = "Modelo de amenazas para API con vulnerabilidad Log4Shell"
+tm.description = "Modelo de amenazas para API PCI"
 
-# Definición de Límites (Boundaries)
+# Límites
 internet = Boundary("Internet")
 trust_zone = Boundary("Trusted Network")
 
-# Elementos de la arquitectura
+# Activos
 user = Server("User Browser")
 user.in_boundary = internet
 
 api_service = Server("Vulnerable API Service")
-api_service.description = "Servicio Java Spring Boot vulnerable a Log4Shell"
 api_service.in_boundary = trust_zone
 api_service.is_web_server = True
 
 db = Datastore("PCI Database")
 db.in_boundary = trust_zone
-db.is_encrypted = False # Esto generará una amenaza automáticamente
 
-# Flujos de datos (Dataflows)
-Dataflow(user, api_service, "HTTPS Traffic (Log4j Payload)")
-Dataflow(api_service, db, "SQL Query / Storage")
+# Flujos
+Dataflow(user, api_service, "HTTPS Traffic")
+Dataflow(api_service, db, "SQL Connection")
 
-# Generar el reporte
+# Esto es lo más importante
 tm.process()
